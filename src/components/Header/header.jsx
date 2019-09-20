39fac4edd70b941b5e48e6fa380c16af8dd8f7ea
Sultan Esbolatov <linpark11197@gmail.com>
2019-09-20 14:34:33 +0600

    подключили бутстрап, начинаем верстать, бекап грида

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import classes from "./header.module.css";
-import logo  from "./logo.png";
-import logo2 from "./Space_logo.svg"
-import {Navbar} from "bootstrap"
+import logo from "./logo.png";
+import logo2 from "./Space_logo.svg";
+import { Navbar } from "react-bootstrap";
 
 const Header = () => {
   return (
-      <nav className="navbar">
-    <header className={classes.head}>
-        <img src={logo2} alt="logo" className={classes.logo}/>
-      <span className={classes.logoName}>logo</span>
-    </header> </nav>
+    <Navbar bg="dark" expand="md">
+      <header className={classes.head}>
+        <img src={logo2} alt="logo" className={classes.logo} />
+        <span className={classes.logoName}>logo</span>
+      </header>
+    </Navbar>
   );
 };
 
